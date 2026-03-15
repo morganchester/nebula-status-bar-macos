8b46907f0f2f50efafe7090a7152306f45a354ee
morgan_chester <morganchester45@gmail.com>
2026-03-15 17:41:17 +0500

    Initial Commit

---
+++ NebulaStatus/NebulaStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NebulaStatusApp.swift
+//  NebulaStatus
+//
+//  Created by morgan chester on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct NebulaStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
